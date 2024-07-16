bb68bc5fd2d419be1c1fa1e62e3e2cdc192cedfd
吳書賢 <neroal@Neroal.local>
2024-07-16 12:24:12 +0800

    優化登入邏輯

--- main.py
+++ main.py
@@ -10,23 +10,27 @@ class Sdk:
 
     def open_login_page(self):
         self.page.get('https://scr.cyc.org.tw/tp01.aspx?module=login_page&files=login')
-        print('open login page')
 
-    def close_entry_button(self):
+    def close_entry_button(self):  
         button = self.page.ele('css:.swal2-confirm.swal2-styled')
         if button:
             button.click()
-            print('close entry button')
 
     def login(self, account, password):
+        time.sleep(3)
+
         self.page('#ContentPlaceHolder1_loginid').input(account)
         self.page('#loginpw').input(password)
-        time.sleep(0.5)
         self.page('#login_but').click()
-        print('login')
+
+        while True:
+            if self.page.url == "https://scr.cyc.org.tw/tp01.aspx?Module=ind&files=ind":
+                break
+            time.sleep(0.5)
+
     def try_booking(self, date, time_slot, d2):
-        self.page.get(f'https://scr.cyc.org.tw/tp01.aspx?module=net_booking&files=booking_place&StepFlag=2&PT=1&D={date}&D2={d2}')
-        
+        self.page.get(f'https://scr.cyc.org.tw/tp01.aspx?module=net_booking&files=booking_place&StepFlag=2&PT=1&D={date}&D2={d2}', timeout=2)
+
         venue_types = ['A', 'B', 'C', 'D']
 
         try:
@@ -44,14 +48,14 @@ class Sdk:
                 return success ? 'Button clicked' : 'Button not found';
             """
             result = self.page.run_js(script)
-            print(result)
+
             if 'Button clicked' in result:
                 return True
         except Exception as e:
             print(f'Error clicking button: {e}')
         return False
 
-    def keep_trying_booking(self, date, time_slot, d2, interval=0.2, max_attempts=10):
+    def keep_trying_booking(self, date, time_slot, d2, interval=0.2, max_attempts=5):
         attempts = 0
         while attempts < max_attempts:
             success = self.try_booking(date, time_slot, d2)
@@ -66,7 +70,6 @@ class Sdk:
 
     def close(self):
         self.page.close()
-        print('close')
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='SDK script with parameters.')
@@ -85,13 +88,10 @@ if __name__ == "__main__":
     now = datetime.now()
     midnight = now.replace(hour=0, minute=0, second=0, microsecond=0) + timedelta(days=1)
     wait_time = (midnight - now).total_seconds()
-    print(f'Waiting for {wait_time} seconds until midnight')
+    two_weeks_later = (midnight + timedelta(weeks=2)).strftime('%Y/%m/%d')
+
     time.sleep(wait_time)
     
-    # Calculate the date two weeks from midnight
-    two_weeks_later = (midnight + timedelta(weeks=2)).strftime('%Y/%m/%d')
-    print(f'Trying to book the venue for {two_weeks_later}')
-
     sdk.keep_trying_booking(two_weeks_later, args.time_slot, args.d2)
     
     sdk.close()
