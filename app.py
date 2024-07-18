03a01ebb8351a9c79b9f5dbead2be7b3677298cf
Neroal <a0987385316@gmail.com>
2024-07-18 13:57:30 +0800

    優化程式
    新增提示訊息
    
    使用執行緒開4個分頁預約場地
    
    優化邏輯

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, render_template, redirect, url_for
 import webbrowser
-import threading
+from threading import Thread, Timer
 from sdk import Sdk
 
 app = Flask(__name__)
@@ -23,14 +23,26 @@ def open():
     webbrowser.open_new('http://127.0.0.1:5000')
 
 def execute(account, password, time_slot):
+    # A: 83, B: 84, C: 1074, D: 1075
+    venue_ids = [83, 84, 1074, 1075]
+
+    threads = []
+    for venue_id in venue_ids:
+        thread = Thread(target=run_sdk, args=(account, password, venue_id, time_slot))
+        threads.append(thread)
+        thread.start()
+
+    for thread in threads:
+        thread.join()
+
+def run_sdk(account, password, venue_id, time_slot):
     bot_sdk = Sdk()
     bot_sdk.open_login_page()
     bot_sdk.close_entry_button()
     bot_sdk.login(account, password)
-    bot_sdk.keep_trying_booking(time_slot)
-    bot_sdk.close()
+    bot_sdk.booking(venue_id, time_slot)
 
 if __name__ == "__main__":
-    threading.Timer(1, open).start()
+    Timer(1, open).start()
 
     app.run()
--- sdk.py
+++ sdk.py
@@ -1,16 +1,16 @@
 # sdk.py
 import time
 from datetime import datetime, timedelta
-from DrissionPage import ChromiumPage
+from DrissionPage import ChromiumPage, ChromiumOptions
 
 class Sdk:
     def __init__(self):
-        self.page = ChromiumPage()
+        co  = ChromiumOptions().auto_port()
+        self.page = ChromiumPage(co)
         self.page.set.auto_handle_alert()
 
     def open_login_page(self):
         self.page.get('https://scr.cyc.org.tw/tp01.aspx?module=login_page&files=login')
-        print('open login page')
 
     def close_entry_button(self):  
         button = self.page.ele('css:.swal2-confirm.swal2-styled')
@@ -18,75 +18,21 @@ class Sdk:
             button.click()
 
     def login(self, account, password):
+        # TODO: Better way to check cloudflare success event
         time.sleep(3)
 
         self.page('#ContentPlaceHolder1_loginid').input(account)
         self.page('#loginpw').input(password)
         self.page('#login_but').click()
-        print('login')
 
-        while True:
-            if self.page.url == "https://scr.cyc.org.tw/tp01.aspx?Module=ind&files=ind":
-                break
-            time.sleep(0.5)
+        self.page.wait.url_change('https://scr.cyc.org.tw/tp01.aspx?Module=ind&files=ind', timeout=10)
 
-    def try_booking(self, date, time_slot, d2):
-        self.page.get(f'https://scr.cyc.org.tw/tp01.aspx?module=net_booking&files=booking_place&StepFlag=2&PT=1&D={date}&D2={d2}', timeout=2)
-
-        venue_types = ['A', 'B', 'C', 'D']
-
-        try:
-            conditions = " || ".join([f'onclickText.includes("羽 {vt}") && onclickText.includes("{time_slot}")' for vt in venue_types])
-            script = f"""
-                var success = false;
-                var buttons = document.getElementsByName('PlaceBtn');
-                for (var i = 0; i < buttons.length; i++) {{
-                    var onclickText = buttons[i].getAttribute('onclick');
-                    if (onclickText && ({conditions})) {{
-                        buttons[i].click();
-                        success = true;
-                    }}
-                }}
-                return success ? 'Button clicked' : 'Button not found';
-            """
-            result = self.page.run_js(script)
-
-            if 'Button clicked' in result:
-                return True
-        except Exception as e:
-            print(f'Error clicking button: {e}')
-        return False
-
-    def keep_trying_booking(self, time_slot, interval=0.2, max_attempts=5):
+    def booking(self, venue_id, time_slot):
         now = datetime.now()
         midnight = now.replace(hour=0, minute=0, second=0, microsecond=0) + timedelta(days=1)
         wait_time = (midnight - now).total_seconds()
         two_weeks_later = (midnight + timedelta(weeks=2)).strftime('%Y/%m/%d')
 
-        print(f'Prepare to book the venue for {two_weeks_later}')
-        print(f'{wait_time} seconds remaining until booking starts')
         time.sleep(wait_time)
 
-        start_time = time_slot.split("~")[0]
-        d2 = 1
-        if "06:00" <= start_time < "11:00":
-            d2 = 1
-        elif "11:00" <= start_time < "17:00":
-            d2 = 2
-        elif "17:00" <= start_time < "22:00":
-            d2 = 3
-
-        attempts = 0
-        while attempts < max_attempts:
-            success = self.try_booking(two_weeks_later, time_slot, d2)
-            if success:
-                print('Successfully booked the venue.')
-                break
-            attempts += 1
-            print(f'Retrying in {interval} seconds... (Attempt {attempts}/{max_attempts})')
-            time.sleep(interval)
-        if attempts == max_attempts:
-            print('Reached maximum attempts. Exiting.')
-
-    def close(self):
-        self.page.close()
+        self.page.get(f'https://scr.cyc.org.tw/tp01.aspx?module=net_booking&files=booking_place&StepFlag=25&QPid={venue_id}&QTime={time_slot}&PT=1&D={two_weeks_later}') 
