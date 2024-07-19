5eb00fd0e33095bbdba02dbc6099de8aea0a7692
Neroal <a0987385316@gmail.com>
2024-07-19 16:18:37 +0800

    實作並發流程 #4

--- sdk.py
+++ sdk.py
@@ -2,12 +2,16 @@
 import time
 from datetime import datetime, timedelta
 from DrissionPage import ChromiumPage, ChromiumOptions
+import requests
+import threading
 
 class Sdk:
     def __init__(self):
         co  = ChromiumOptions().auto_port()
         self.page = ChromiumPage(co)
         self.page.set.auto_handle_alert()
+        self.cookies = {}
+        self.venus_ids = [83, 84, 1074, 1075]
 
     def open_login_page(self):
         self.page.get('https://scr.cyc.org.tw/tp01.aspx?module=login_page&files=login')
@@ -26,13 +30,33 @@ class Sdk:
         self.page('#login_but').click()
 
         self.page.wait.url_change('https://scr.cyc.org.tw/tp01.aspx?Module=ind&files=ind', timeout=10)
-
-    def booking(self, venue_id, time_slot):
+        
+        self.cookies = self.page.cookies()
+    
+    def booking(self, time_slot):
         now = datetime.now()
         midnight = now.replace(hour=0, minute=0, second=0, microsecond=0) + timedelta(days=1)
         wait_time = (midnight - now).total_seconds()
         two_weeks_later = (midnight + timedelta(weeks=2)).strftime('%Y/%m/%d')
 
+        # mark for fast testing
         time.sleep(wait_time)
 
-        self.page.get(f'https://scr.cyc.org.tw/tp01.aspx?module=net_booking&files=booking_place&StepFlag=25&QPid={venue_id}&QTime={time_slot}&PT=1&D={two_weeks_later}') 
+        threads = []
+        for _ in range(10):
+            for id in self.venus_ids:
+                url = f'https://scr.cyc.org.tw/tp01.aspx?module=net_booking&files=booking_place&StepFlag=25&QPid={id}&QTime={time_slot}&PT=1&D={two_weeks_later}'
+                thread = threading.Thread(target=self.fetch, args=(url,))
+                threads.append(thread)
+                thread.start()
+
+        for thread in threads:
+            thread.join()
+
+    def fetch(self, url):
+        # print('fetch:', url)
+        session = requests.Session()
+        for cookie in self.cookies:
+            session.cookies.set(cookie['name'], cookie['value'])
+        
+        response = session.get(url)
